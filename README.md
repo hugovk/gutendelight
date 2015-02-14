208a09133978e5997b8e8c6bfc14c4fa286fd6ac
hugovk <hugovk@users.noreply.github.com>
2015-02-14 23:02:51 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# gutenrap
-Tweeting a mix of hip hop (Rapper's Delight, Fight the Power, The Message, etc.) and classic literature from Project Gutenberg.
+# @gutendelight
+
+Tweeting a mix of hip-hop (Rapper's Delight, Fight the Power, The Message, etc.) and classic literature from Project Gutenberg.
+
+See the Twitter bot in action at **[![](https://abs.twimg.com/favicons/favicon.ico)@gutendelight](https://twitter.com/gutendelight)** and some of the best at [![](http://favstar.fm/favicon.ico)http://favstar.fm/users/gutendelight](http://favstar.fm/users/gutendelight).
+
+## Some examples
+
+> The brother who rocks so viciously
+> He gazed at me suspiciously
+
+https://twitter.com/hugovk/status/564387719259111425
+
+> He said, "Sit down, punk, I wanna talk to you
+> Each has his lot, and bears the fate he drew.
+
+https://twitter.com/gutendelight/status/566563697787940865
+
+I guess by now you can take a hunch
+"Let us consider," said he, after lunch.
+
+https://twitter.com/gutendelight/status/566563143791673344
+
+Then you throw your hands high in the air,
+" "I don't see," said the native Bear.
+
+https://twitter.com/gutendelight/status/566560879475687425
+
+## Algorithm goes a little something like this
+
+1. Pick a random line from a file containing a bunch of hip-hop lyrics.
+2. Take the last word and find its rhymes using the [Wordnik API](http://developer.wordnik.com/docs.html#!/word/getRelatedWords_get_4).
+3. Pick a rhyme at random.
+4. Find all the sentences from the [Project Gutenberg August 2003 CD](http://www.gutenberg.org/wiki/Gutenberg:The_CD_and_DVD_Project) ("contains 600 of our best Ebooks") that end with that rhyming word (using [gutengrep](https://github.com/hugovk/gutengrep)).
+5. Pick one that's about the same number of syllables as the first line (using [https://github.com/akkana/scripts/blob/master/countsyl](Akkana Peck's countsyl).
+6. If no rhymes or matches found, or a sentence is too long, try another until something's found.
+7. Tweet 'em!
---
+++ gutendelight.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Tweet a random line of rap coupled with a rhyming line from Project Gutenberg.
+"""
+from __future__ import print_function, unicode_literals
+import argparse
+import countsyl  # https://github.com/akkana/scripts/blob/master/countsyl
+import gutengrep
+import random
+import re
+import sys
+import twitter
+import webbrowser
+import yaml
+
+from pprint import pprint
+
+from wordnik import swagger, WordApi
+
+TWITTER = None
+
+
+# Windows cmd.exe cannot do Unicode so encode first
+def print_it(text):
+    print(text.encode('utf-8'))
+
+
+def load_text(infile):
+    """Return a list of non-blank lines, ignore those beginning #"""
+    with open(infile) as f:
+        lines = f.read().splitlines()
+    return list(line for line in lines if line and not line.startswith("#"))
+
+
+def get_rhymes_from_wordnik(word):
+    global data
+    wordnik_client = swagger.ApiClient(data['wordnik_api_key'],
+                                       'http://api.wordnik.com/v4')
+    wordApi = WordApi.WordApi(wordnik_client)
+    rhymes = wordApi.getRelatedWords(word, relationshipTypes="rhyme")
+    return rhymes
+
+
+def get_matches_from_project_gutenberg(word, sentences):
+    """Find a matching sentence ending with this word"""
+
+    # Filter
+    flags = re.IGNORECASE  # TODO try with case first? nah...
+#     regex = "\b" + word + "\b\W*$"  # word followed by non-letters and $
+    regex = word  # word followed by non-letters and $
+    regex = word + "\b\W*$"  # word followed by non-letters and $
+    regex = r"\b" + word + r"\b\W*$"  # word followed by non-letters and $
+    print(regex)
+    pprint(regex)
+    sentences = gutengrep.find_matching_sentences(
+        regex, sentences,
+        flags=flags)
+
+    return sentences
+
+
+def number_of_syllables(sentence):
+    min_syllables, max_syllables = countsyl.count_syllables(sentence)
+    syllables = (min_syllables + max_syllables) / 2.0
+    return syllables
+
+
+def gutendelight(rapfile, inspec, cache):
+    rap = load_text(rapfile)
+#     pprint(rap)
+
+    # Prep PG
+    all_sentences = gutengrep.prepare(inspec, cache)
+
+    while(True):
+
+        random_line = random.choice(rap)
+        print(random_line)
+
+        last_word = rrandom_line.split()[-1]
+        print(last_word)
+
+        rhymes = get_rhymes_from_wordnik(last_word)
+        if not rhymes:
+            print("No rhymes found, try next line of rap")
+            # TODO try canonical? or just continue and find a better one?
+            continue
+
+        rhymes = rhymes[0].words
+        pprint(rhymes)
+
+        random.shuffle(rhymes)
+        pprint(rhymes)
+
+        random_line_syllables = number_of_syllables(random_line)
+
+        for rhyme in rhymes:
+            print(rhyme)
+
+            # Find rhyming sentences from Project Gutenberg
+            sentences = get_matches_from_project_gutenberg(rhyme,
+                                                           all_sentences)
+            if not sentences:
+                print("No matches found, try next rhyme")
+                continue
+
+            pprint(sentences)
+            closest_diff = 999
+            best = None
+            for sentence in sentences:
+                sentence = sentence.replace("\r\n", " ")  # Remove newlines
+                sentence = " ".join(sentence.split())  # Remove dupe spaces
+                output = random_line + "\n\n" + sentence
+                print("*"*80)
+                print_it(output)
+                print(len(output))
+                if len(output) > 140:
+                    print("Too long, try next sentence")
+                    continue
+                else:
+                    # Pick one that's about the same number of syllables
+                    syllables = number_of_syllables(sentence)
+                    diff = abs(random_line_syllables - syllables)
+                    print(diff, random_line_syllables, syllables)
+                    if diff < closest_diff:
+                        closest_diff = diff
+                        best = output
+            if best:
+                print("Bingo!")
+                return best
+
+
+def load_yaml(filename):
+    """
+    File should contain:
+    consumer_key: TODO_ENTER_YOURS
+    consumer_secret: TODO_ENTER_YOURS
+    access_token: TODO_ENTER_YOURS
+    access_token_secret: TODO_ENTER_YOURS
+    wordnik_api_key: TODO_ENTER_YOURS
+    """
+    f = open(filename)
+    data = yaml.safe_load(f)
+    f.close()
+    if not data.viewkeys() >= {
+            'access_token', 'access_token_secret',
+            'consumer_key', 'consumer_secret'}:
+        sys.exit("Twitter credentials missing from YAML: " + filename)
+    if not data.viewkeys() >= {
+            'wordnik_api_key'}:
+        sys.exit("Wordnik credentials missing from YAML: " + filename)
+    return data
+
+
+def tweet_it(string, in_reply_to_status_id=None):
+    global TWITTER
+
+    if len(string) <= 0:
+        print("ERROR: trying to tweet an empty tweet!")
+        return
+
+    # Create and authorise an app with (read and) write access at:
+    # https://dev.twitter.com/apps/new
+    # Store credentials in YAML file
+    if TWITTER is None:
+        TWITTER = twitter.Twitter(auth=twitter.OAuth(
+            data['access_token'],
+            data['access_token_secret'],
+            data['consumer_key'],
+            data['consumer_secret']))
+
+    print_it("TWEETING THIS: " + string)
+
+    if args.test:
+        print("(Test mode, not actually tweeting)")
+    else:
+        print("POST statuses/update")
+        result = TWITTER.statuses.update(
+            status=string,
+            in_reply_to_status_id=in_reply_to_status_id)
+        url = "http://twitter.com/" + \
+            result['user']['screen_name'] + "/status/" + result['id_str']
+        print("Tweeted: " + url)
+        if not args.no_web:
+            webbrowser.open(url, new=2)  # 2 = open in a new tab, if possible
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Tweet a random line of rap coupled with"
+                    "a rhyming line from Project Gutenberg.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-r', '--rap',
+        default='/Users/hugo/Dropbox/txt/raplines.txt',
+        help="A text file of rap lyrics.")
+    parser.add_argument('inspec', nargs='?',
+                        help="Input file spec")
+    parser.add_argument('--cache', action='store_true',
+                        help="Load cache. If no cache, save one. Warning: "
+                             "the cache is saved based on the initial "
+                             "inspec. Subsequent uses are based on this "
+                             "initial cache, effectively ignoring inspec. ")
+    parser.add_argument(
+        '-y', '--yaml',
+        default='/Users/hugo/Dropbox/bin/data/gutendelight.yaml',
+        # default='E:/Users/hugovk/Dropbox/bin/data/gutendelight.yaml',
+        help="YAML file containing Twitter and Wordnik keys and secrets")
+    parser.add_argument(
+        '-nw', '--no-web', action='store_true',
+        help="Don't open a web browser to show the tweeted tweet")
+    parser.add_argument(
+        '-x', '--test', action='store_true',
+        help="Test mode: go through the motions but don't update anything")
+    args = parser.parse_args()
+
+    data = load_yaml(args.yaml)
+
+    output = gutendelight(args.rap, args.inspec, args.cache)
+    print("="*80)
+    print(output)
+    print("="*80)
+
+    print("Tweet this:\n" + output)
+    try:
+        tweet_it(output)
+
+    except twitter.api.TwitterHTTPError as e:
+        print("*"*80)
+        print(e)
+        print("*"*80)
+
+
+# End of file
