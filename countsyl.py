9bff43dccc557ff5f3460f0927ee6c5e2f9b09b9
Hugo <hugovk@users.noreply.github.com>
2018-11-14 11:57:19 +0200

    Flake8 + Black

--- countsyl.py
+++ countsyl.py
@@ -19,8 +19,9 @@ import sys
 
 verbose = False
 
+
 def count_syllables(word):
-    vowels = ['a', 'e', 'i', 'o', 'u']
+    vowels = ["a", "e", "i", "o", "u"]
 
     on_vowel = False
     in_diphthong = False
@@ -32,50 +33,55 @@ def count_syllables(word):
     for c in word:
         is_vowel = c in vowels
 
-        if on_vowel == None:
+        if on_vowel is None:
             on_vowel = is_vowel
 
         # y is a special case
-        if c == 'y':
+        if c == "y":
             is_vowel = not on_vowel
 
         if is_vowel:
-            if verbose: print c, "is a vowel"
+            if verbose:
+                print(c, "is a vowel")
             if not on_vowel:
                 # We weren't on a vowel before.
                 # Seeing a new vowel bumps the syllable count.
-                if verbose: print "new syllable"
+                if verbose:
+                    print("new syllable")
                 minsyl += 1
                 maxsyl += 1
             elif on_vowel and not in_diphthong and c != lastchar:
                 # We were already in a vowel.
                 # Don't increment anything except the max count,
                 # and only do that once per diphthong.
-                if verbose: print c, "is a diphthong"
+                if verbose:
+                    print(c, "is a diphthong")
                 in_diphthong = True
                 maxsyl += 1
-        elif verbose: print "[consonant]"
+        elif verbose:
+            print("[consonant]")
 
         on_vowel = is_vowel
         lastchar = c
 
     # Some special cases:
-    if word[-1] == 'e':
+    if word[-1] == "e":
         minsyl -= 1
     # if it ended with a consonant followed by y, count that as a syllable.
-    if word[-1] == 'y' and not on_vowel:
+    if word[-1] == "y" and not on_vowel:
         maxsyl += 1
 
     return minsyl, maxsyl
 
-if __name__ == '__main__':
-    if sys.argv[1].startswith('-f'):
+
+if __name__ == "__main__":
+    if sys.argv[1].startswith("-f"):
         for word in open(sys.argv[2]):
             word = word.strip()
             smax, smin = count_syllables(word)
-            print word, smax, smin
+            print(word, smax, smin)
     else:
         for word in sys.argv[1:]:
             word = word.strip()
             smax, smin = count_syllables(word)
-            print word, smax, smin
+            print(word, smax, smin)
